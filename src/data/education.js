2aef311b8da68d6c10dbc5fca8a71bc1e5bb5eb9
kabirou koulika <kabiroukoulika@gmail.com>
2026-09-19 02:11:07 +0100

    voilà

--- src/data/education.js
+++ src/data/education.js
@@ -1,19 +1,39 @@
 export const education = [
   {
+    id: "edu-2",
+    period: "2022 - 2023",
+    status: "obtenu",
+    degree: "BACCALAUREAT serie C",
+    school: "CEG1 Dassa-Zoumè",
+    description: "Obtention d'une socle solide en  mathématique et en physique, qui ont été des bases importantes pour ma future formation en informatique.",
+  },
+  {
     id: "edu-1",
-    period: "[ADD_ANNEES] — ex : 2024 → 2027",
+    period: "2023 → 2024",
     status: "en cours",
-    degree: "[ADD_INTITULE_FORMATION] — ex : Licence Informatique",
-    school: "IFRI — Institut de Formation et de Recherche en Informatique de l'UAC",
+    degree: "Licence 1 en Mathématique Informatique et Application",
+    school: "FAST - Faculté des Sciences et Techniques de l'Université d'Abomey-Calavi",
+    description:
+      "Formation axée sur la mathématique, l'informatique, les systèmes et les fondamentaux nécessaires à une pratique technique solide.",
+  },
+
+  {
+    id: "edu-4",
+    period: "2024 → 2025",
+    status: "en cours",
+    degree: "Licence 2 en Mathématique Informatique et Application",
+    school: "FAST - Faculté des Sciences et Techniques de l'Université d'Abomey-Calavi",
     description:
       "Formation axée sur l'informatique, couvrant le développement logiciel, les systèmes et les fondamentaux nécessaires à une pratique technique solide.",
   },
+
   {
-    id: "edu-2",
-    period: "[ADD_ANNEES_PRECEDENTES]",
-    status: "obtenu",
-    degree: "[ADD_DIPLOME_PRECEDENT] — ex : Baccalauréat scientifique",
-    school: "[ADD_ETABLISSEMENT]",
-    description: "[ADD_DESCRIPTION_COURTE]",
+    id: "edu-3",
+    period: "2025 → 2026",
+    status: "en cours",
+    degree: "Licence 1 en Sécurité Informatique",
+    school: "IFRI — Institut de Formation et de Recherche en Informatique de l'UAC",
+    description:
+      "Formation axée sur la Cybersécurité et les réseaux",
   },
 ];
